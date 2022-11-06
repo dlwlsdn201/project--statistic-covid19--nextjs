2bfaa5137c6708198d25ecffbbe7f9d3d4e26c0f
leejinw <dlwlsdn201@naver.com>
2022-11-06 13:29:46 +0900

    :hammer: 404 에러 page 파일 생성

---
+++ pages/404.tsx
@@ -0,0 +1,15 @@
+import React from 'react';
+
+const Error404 = () => {
+	return (
+		<div>
+			<div>
+				<strong>
+					<h2>404 - Page not found</h2>
+				</strong>
+			</div>
+		</div>
+	);
+};
+
+export default Error404;
