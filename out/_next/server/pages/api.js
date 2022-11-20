7590c2964e2b22968f9ef7316304949820373c5f
leejinw <dlwlsdn201@naver.com>
2022-11-20 16:47:46 +0900

    deploy to gh-pages

---
+++ out/_next/server/pages/api.js
@@ -0,0 +1,25 @@
+"use strict";
+(() => {
+var exports = {};
+exports.id = 237;
+exports.ids = [237];
+exports.modules = {
+
+/***/ 2167:
+/***/ ((module) => {
+
+module.exports = require("axios");
+
+/***/ })
+
+};
+;
+
+// load runtime
+var __webpack_require__ = require("../webpack-runtime.js");
+__webpack_require__.C(exports);
+var __webpack_exec__ = (moduleId) => (__webpack_require__(__webpack_require__.s = moduleId))
+var __webpack_exports__ = __webpack_require__.X(0, [939], () => (__webpack_exec__(939)));
+module.exports = __webpack_exports__;
+
+})();
