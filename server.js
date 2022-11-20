1503d39c8976fa45850448234db0cf59f9d56f44
leejinw <dlwlsdn201@naver.com>
2022-11-20 15:56:13 +0900

    :hammer: next 프레임워크의 server.js 파일 생성

---
+++ server.js
@@ -0,0 +1,19 @@
+const express = require('express');
+const next = require('next');
+
+const dev = process.env.NODE_ENV !== 'production'; //false => product, true => dev
+const app = next({ dev });
+const handle = app.getRequestHandler();
+
+app.prepare().then(() => {
+	const server = express();
+	//필수항목!, pages폴더 아래 component들 pre- render
+	server.get('*', (req, res) => {
+		return handle(req, res);
+	});
+
+	server.listen(3000, (err) => {
+		if (err) throw err;
+		console.log('> Ready on http://localhost:3000');
+	});
+});
