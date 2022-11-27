ea2c125f89ce602f7339970ee9eb573590673266
leejinw <dlwlsdn201@naver.com>
2022-11-27 14:31:52 +0900

    :zap: resolved #16 초기 통계 데이터 불러오는 로직 방식 변경 (SSR -> CSR)

--- pages/_app.tsx
+++ pages/_app.tsx
@@ -18,4 +18,19 @@ function MyApp({ Component, pageProps }: AppProps) {
 	);
 }
 
+// getServerSideProps 가 호출되었을 때, App 의 getInitialProps 가 재호출 되는 이슈를 막기 위함
+// MyApp.getInitialProps = async (appContext: any) => {
+// 	const appProps = await App.getInitialProps(appContext);
+// 	const {
+// 		ctx: { req }
+// 	} = appContext;
+
+// 	if (req?.url.startsWith('/_next')) {
+// 		// serverSideProps로 호출된 경우 URL이 /_next로 시작함.
+// 		// EX: /_next/data/development/index.json
+// 	}
+
+// 	return { ...appProps };
+// };
+
 export default MyApp;
