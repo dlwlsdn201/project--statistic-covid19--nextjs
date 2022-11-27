64772ac42f68697de689ffee5820872e83ac8814
leejinw <dlwlsdn201@naver.com>
2022-11-27 17:17:06 +0900

    :sparkles: resolved #20 일일 API 조회 횟수 초과할 경우, 예외 처리 구현 완료

--- components/Modules/Notification.tsx
+++ components/Modules/Notification.tsx
@@ -2,14 +2,21 @@ import { FrownTwoTone, SmileTwoTone } from '@ant-design/icons';
 import { notification } from 'antd';
 import React from 'react';
 
-const CustomNotification = ({ result, errorMsg = undefined }) => {
+type TArgs = {
+	result: object | undefined;
+	resultCode?: number;
+	errorMsg?: string;
+};
+
+const CustomNotification = ({ result, resultCode, errorMsg }: TArgs): void => {
 	let props = {
 		message: '데이터 호출을 성공하였습니다',
 		placement: 'topRight',
+		description: <span />,
 		icon: <SmileTwoTone style={{ fontSize: 32 }} />
 	};
 	if (!result) {
-		const description = (
+		let description = (
 			<div>
 				<h4 style={{ marginBottom: '1.5em' }}>
 					원인 : <span style={{ color: 'red' }}>{`${errorMsg}`}</span>
@@ -20,15 +27,24 @@ const CustomNotification = ({ result, errorMsg = undefined }) => {
 			</div>
 		);
 
+		if (resultCode === 22) {
+			// 일일 조회 횟수 초과 시, 코드
+			description = (
+				<div>
+					<p style={{ marginTop: '1.5em' }}>{errorMsg}</p>
+				</div>
+			);
+		}
+
 		props = {
 			...props,
 			message: '데이터 호출을 실패하였습니다.',
-			description: description,
+			description,
 			icon: <FrownTwoTone style={{ fontSize: 32 }} />
 		};
 	}
 
-	const openNotification = (Attributes) => {
+	const openNotification = (Attributes: any) => {
 		notification.open({ ...Attributes, duration: 3 });
 	};
 
